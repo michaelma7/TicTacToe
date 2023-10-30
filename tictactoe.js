84a4f2cef7f3435fbff202341f0e6ac634844fc9
michaelma7 <michaelma16688@gmail.com>
2023-10-30 14:40:29 -0400

    day 1 add ins html added a modal form to add new player created empty gameboard, missing css
    js
    added DOM cache for buttons and modal form
    added event listeners
    removed gameboard creation
    added iife for non game module

--- tictactoe.js
+++ tictactoe.js
@@ -1,25 +1,95 @@
-//game board module
-const gameBoard = (function () {
-    const row1 = ["", "" ,""];
-    const row2 = ["", "" ,""];
-    const row3 = ["", "" ,""];
-    const emptyBoard = [];
-    emptyBoard.push(row1).push(row2).push(row3);
-    return {emptyBoard};
+//game controller
+const gameController = (function() {
+    //decide whether to have empty board in html or render via js
+    // function gameBoard() {
+    //     const emptyBoard = [];
+    //     for(let x=0; x<3; x++){
+    //         for(let y=0; y<3; y++){
+    //             addCell(gameBoard, x, y);
+    //         };
+    //     }
+    //     function addCell(map, rows, columns){
+    //         map[x][y] = '';
+    //     }
+    //     render(emptyBoard);
+    // };
+
+    //cache DOM
+    const form = document.querySelector('.playercreation');
+    const modal = document.querySelector('dialog');
+    const addNew = document.querySelector('.newplayer');
+    const playerName = document.querySelector('#name');
+    const playerSign = document.querySelector('#sign');
+    const create = document.querySelector('.createplayer');
+    const reset = document.querySelector('.reset');
+    const start = document.querySelector('.start');
+    const AI = document.querySelector('.AI');
+
+    //event listeners
+    addNew.addEventListener('click', modal.showModal())
+    create.addEventListener('click', (e) => {
+                                                e.preventDefault();
+                                                createPlayer(playerName.value , playerSign.value);
+                                                modal.close();
+                                            });
+    reset.addEventListener('click', (e) => restart());
+    start.addEventListener('click', (e) => startGame();)
+    AI.addEventListener('click', playComputer());
+
+    function render(boardState) {
+        // const target = document.querySelector('.gameboard');
+        // if (!target.haschildNode()) {
+        //     target.removeChild(target.firstChild);
+        //     target.appendChild(boardState);
+        // } else {
+        //     target.appendChild(boardState);
+        // };      
+    };
+
+    function checkWin() {
+        if(three in a row check rows, columns and two diags){
+
+        };
+    };
+
+    function boardMove (player, position) {
+        position.innerHTML() = player.sign;
+        checkWin();
+        render(currentState);    
+    };
+
+    function startGame() {
+
+    };
+
+    function restart() {
+
+    };
+
+    function playComputer() {
+
+    };
+
+    //player creation
+    function createPlayer (name, sign) {
+        let wins = 0;
+        const getWins = () => wins;
+        const won = () => win++;
+        form.reset();
+        //add render to DOM
+        render();
+        return { name, sign, wins, getWins, won };
+    };
+
+    return { boardMove, createPlayer, startGame, playComputer, reset };
 })();
 
-//display controller
-function boardMove (player, position) {
-    position.innerHTML() = player.sign;    
-    return {};
-}
+//player controller is this necessary?
+const playerController = (function() {
 
-//player creation
-function createPlayer (name, sign) {
-    let wins = 0;
-    const getWins = () => wins;
-    const won = () => win++;
-    return {name, sign, wins};
-}
+})();
+
+
+
 
 
