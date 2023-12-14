e18bc04c2faaaf1194de65f5b227d7b4a69c55db
michaelma7 <michaelma16688@gmail.com>
2023-12-14 09:41:55 -0500

    post vacation commit
    html changes
    added modal for starting game
    added a div for showing turn order
    removed checked default from player creation modal
    
    added svg for 'x' sign on modal
    
    js changes
    added random seed generator to determine who goes first
    added turn order function to boardController to determine turn order
    changed board to use words instead of blanks
    changed checkWin to accept a sign to check against and made variables more descriptive
    added render method to playerController add player info to page
    added deleteplayer method
    startGame function filled out
    added modal controls to gameController for startGame function trigger

--- tictactoe.js
+++ tictactoe.js
@@ -4,13 +4,24 @@ const boardController = (function() {
     const gameboard = document.querySelector('.gameboard');
     const nodes = gameboard.childNodes;
     const squareArray = Array.from(nodes); //es6 only
+    const turnOrder = document.querySelector('.player-turn');
 
-    //event listeners
-    squareArray.forEach(place => place.addEventListener('click', (e) => boardMove(player, e.target))); // move to game start function? how to disable during player creation
+    //event listeners how to make this activate only once game has started. have it called via game start?
+    squareArray.forEach(place => place.addEventListener('click', (e) => {boardMove(turnOrder.lastChild.innerHTML, e.target); turnControl();}));
+    
+    //other variables
+    let playerTurn = '';
+    const seed = Math.floor(Math.random()*2);
+    if(seed<1){
+        //player one goes first
+        playerTurn = true;
+    } else {
+        //player two goes first
+        playerTurn = false; 
+    };
 
     //gameboard variables and creation
-    let gameStart = false;
-    let board = ['', '', '', '', '', '', '', '', ''];
+    let board = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let winningCombos = {};
     for(let i=0; i<3; i++) {
         winningCombos["row"+(i+1)] = [i*3, (i*3)+1, (i*3)+2];
@@ -22,36 +33,38 @@ const boardController = (function() {
     winningCombos["diag2"] = [2, 4, 6];
 
     function render(sign, position) {
-        position.innerHTML = sign;
-        //if some win condition then open modal?   
+        position.innerHTML = sign; 
     };
 
-    function checkWin() {
-        //check for ties
-        if(!board.includes('')) {
-            return "Tie!";
+    function checkWin(sign) {
+        //check for tie. loop through all children on gameboard to check if they have children
+        for(let div in squareArray) {
+            if(!div.hasChildNodes()) {
+                continue;
+            } else {
+                return "Tie";
+            };
         };
-        //map current positions
-        let xMap = board.map((position) => position === 'X');
-        let oMap = board.map((position) => position === 'O'); 
+            
+        //map position of selected sign
+        let signMap = board.map((position) => position === sign);
+        //check each winning condition vs current state 
         for(let combo in winningCombos) {
             let condition = winningCombos[combo];
-            let x = [];
-            let y = [];
+            let line = [];
             for(let location of condition) {
-              x.push(xMap[location]);
-              y.push(oMap[location]);
+              line.push(signMap[location]);
             };
-            if(!x.includes(false) || y.includes(!false)){
-                return "Win!";
+            if(!line.includes(false)){
+                //push to game controller to render winning player
+                gameController.render(playerName);
             };
-        };
-        //push this to another render function that opens a modal for winning situation
+        };        
     };
 
     function boardMove(sign, position) {
         //check for game start and empty square
-        if(!gameStart) {
+        if(!gameController.startStatus) {
             return "Game has not started!";
         } else if (position.innerHTML !== '') {
             return "Position has been filled!";
@@ -61,14 +74,21 @@ const boardController = (function() {
                     board[k] = sign;
                 };
             };
-            //check for win
             render(sign, position);
-            checkWin();
+            checkWin(sign);
         };
     };
 
+    function turnControl() {
+        if(playerTurn) {
+            turnOrder.innerHTML = playerController.playerOne.innerHTML;
+        } else {
+            turnOrder.innerHTML = playerController.playerTwo.innerHTML;
+        };
+        playerTurn = !playerTurn;
+    };
 
-    return { boardMove, checkWin};
+    return {boardMove, checkWin, turnControl};
 })();
 
 const playerController = (function() {
@@ -78,19 +98,44 @@ const playerController = (function() {
     const modal = document.querySelector('.creationdialog');
     const addNew = document.querySelector('.newplayer');
     const playerName = document.querySelector('#name');
-    const playerSign = document.querySelector('input[name=sign]:checked');
     const create = document.querySelector('.createplayer');
+    const playerOne = document.querySelector('.playerone');
+    const playerTwo = document.querySelector('.playertwo');
 
     //event listeners
     addNew.addEventListener('click', (e) => modal.showModal());
     create.addEventListener('click', (e) => {
                                                 e.preventDefault();
-                                                createPlayer(playerName.value , playerSign.value);
+                                                createPlayer(playerName.value , document.querySelector('input[name=sign]:checked').value);
                                                 modal.close();
                                             });
                                     
     function render(name, sign) {
-        
+        //check which div is empty
+        if(!playerOne.hasChildNodes()) {
+            //create div with name
+            let player = document.createElement("span");
+            player.className = `${name}`;
+            player.innerHTML = "Name: " + `${name}`;
+            //create div with sign
+            let symbol = document.createElement('div');
+            symbol.className = `${sign}`;
+            symbol.innerHTML = "Sign: " + `${sign}`;
+            playerOne.appendChild(player);
+            playerOne.appendChild(symbol);
+        //player two check and add
+        } else if(!playerTwo.hasChildNodes()) {
+            let player2 = document.createElement("span");
+            player2.className = `${name}`;
+            player2.innerHTML = name;
+            let symbol2 = document.createElement('div');
+            symbol2.className = `${sign}`;
+            symbol2.innerHTML = 'Sign: ' +`${sign}`;
+            playerTwo.appendChild(player2);
+            playerTwo.appendChild(symbol2);
+        } else {
+            return "Nothing to render.";
+        };
     };
 
     //player creation
@@ -99,23 +144,22 @@ const playerController = (function() {
         const getWins = () => wins;
         const won = () => win++;
         form.reset();
-        //add player to DOM via another function
         render(name, sign);
         return { name, sign, wins, getWins, won };
-    };
-
-    function addPlayer(name, sign) {
-
     };
 
     function deletePlayer(name) {
         //find player and delete
         let player = document.querySelector(`${name}`);
-        player.innerHTML = '';
-        
+        player.removeChild(player.firstChild());
+        player.removeChild(player.firstChild());
     };
 
-    return {createPlayer, deletePlayer};
+    function winningPlayer() {
+
+    };
+
+    return {createPlayer, deletePlayer, winningPlayer, playerOne, playerTwo};
 })();
 
 const gameController = (function(){
@@ -123,35 +167,51 @@ const gameController = (function(){
     const reset = document.querySelector('.reset');
     const start = document.querySelector('.start');
     const AI = document.querySelector('.AI');
+    const startModal = document.querySelector('.startdialog');
+    const overlay = document.querySelector('.overlay');
+    const closeModal = document.querySelector('.modal-close');
+    const gameStart = document.querySelector('.modal-submit');
 
     //eventlisteners
     reset.addEventListener('click', (e) => restart());
     start.addEventListener('click', (e) => startGame());
     AI.addEventListener('click', (e) => playComputer());
+    closeModal.addEventListener('click', (e) => startModal.close());
+    gameStart.addEventListener('click', (e) => {e.preventDefault(); boardController.turnControl(); startModal.close();});
+
+    //other variables
+    let startStatus = false;
 
     function startGame() {
-        gameStart = true;
-        //set game start to true
-        //take the two players a select who goes first randomly or add an option to select who goes first
-        //allow players to then interact with board and add signs
-        //call a function to notify users to start placing signs
+
+        //check that there are two players
+        if(!playerController.playerTwo.hasChildNodes || !playerController.playerOne.hasChildNodes) {
+            return console.log("Not enough players");
+        } else {
+            startStatus = true;
+        };
+        //open modal to confirm game start
+        startModal.showModal();
+        //push to turn order function
     };
 
     function restart() {
         window.location.reload();
     };
 
+    function render(name) {
+        startStatus = false;
+        return `${name}` + " Wins!";
+    };
+
     function playComputer() {
         //create player with name computer and random sign or sign not taken
+        playerController.createPlayer('Computer', sign);
         //start game
         startGame();
     };
 
-    function render() {
-
-    };
-
-    return {startGame, playComputer, restart};
+    return {startStatus, restart};
 })();
 
 
